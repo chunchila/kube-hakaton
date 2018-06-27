98937609be5a4fe7d0505d451e0b50b4f30e6ab3
romanrozin <r@commit.com>
2018-06-27 13:43:01 +0300

    rr

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -15,11 +16,19 @@ func greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	postMe("http://40.113.149.11/v1/order/")
+
+	var wg sync.WaitGroup
+	for index := 0; index < 100; index++ {
+		wg.Add(1)
+		go postMe("http://40.113.149.11/v1/order/", &wg, index)
+	}
+
+	wg.Wait()
 }
 
-func postMe(url string) {
+func postMe(url string, wg *sync.WaitGroup, index int) {
 
+	defer wg.Done()
 	var jsonStr = []byte(`"{"EmailAddress": "roman",  "ID": "string",  "PreferredLanguage": "string",  "Product": "string",  "Partition": "string",  "Source": "string",  "Status": "strinfgvfdvdfsvfg",  "Total": 1}"`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("accept", "application/json")
@@ -32,8 +41,8 @@ func postMe(url string) {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	fmt.Println("response Status:", resp.Status, index)
+	//fmt.Println("response Headers:", resp.Header)
 	//body, _ := ioutil.ReadAll(resp.Body)
 	//fmt.Println("response Body:", string(body))
 }
